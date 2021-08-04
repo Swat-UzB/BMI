8ae03b0cd59f5a2d60e86555f4a8bf320879c430
Swat-UzB <61419443+Swat-UzB@users.noreply.github.com>
2021-08-04 18:36:06 +0500

    first

---
+++ app/src/main/java/com/example/bmiapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bmiapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bmiapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root )
+    }
+}
